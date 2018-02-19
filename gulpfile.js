f1d079941f0b02f3aeab3b5a23ef29fab258f090
Timo Sorakivi <timo.sorakivi@gmail.com>
2018-02-19 19:15:40 +0200

    added mixins

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,12 @@
-var gulp = require('gulp');
-var watch = require('gulp-watch');
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
-var cssvars = require('postcss-simple-vars');
-var nested = require('postcss-nested');
-var cssImport = require('postcss-import');
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
-gulp.task('default', function () {
-	console.log('a gulp task created.');
-});
 
-gulp.task('html', function () {
-	console.log('something useful being done to your html files.');
-});
 
-gulp.task('styles', function () {
-	return gulp.src('./app/assets/styles/styles.css')
-		.pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
-		.pipe(gulp.dest('./app/temp/styles'));
-});
 
-gulp.task('watch', function () {
-	watch('./app/index.html', function () {
-		gulp.start('html');
-	});
 
-	watch('./app/assets/styles/**/*.css', function () {
-		gulp.start('styles');
-	});
 
-});
+
+
+
 
